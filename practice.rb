63c502f597a7eca30135f3e812d56e3b88353de1
Phillip Cumberlander <cumberlander11@gmail.com>
2018-11-26 11:23:26 -0500

    created a triangle using ruby

--- practice.rb
+++ practice.rb
@@ -1,1 +1,8 @@
+puts "Will add a automatic line break"
+# Puts will add a new line after text...while print will put everything on same line
 print "Hello world"
+
+puts "   / |"
+puts "  /  |"
+puts " /   |"
+puts "/____|"
