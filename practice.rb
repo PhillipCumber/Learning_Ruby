f2d87649e457c0916b6bb103a85e37da8e94306a
Phillip Cumberlander <cumberlander11@gmail.com>
2018-11-26 11:35:13 -0500

    worked on learning variables

--- practice.rb
+++ practice.rb
@@ -1,8 +1,16 @@
 puts "Will add a automatic line break"
 # Puts will add a new line after text...while print will put everything on same line
-print "Hello world"
+# print "Hello world"
 
-puts "   / |"
-puts "  /  |"
-puts " /   |"
-puts "/____|"
+# puts "   / |"
+# puts "  /  |"
+# puts " /   |"
+# puts "/____|"
+
+char_name = "Phillip"
+char_age = "26"
+
+puts char_name + " is a young bull"
+puts "He is almost " + char_age + " years old"
+puts char_name + " whats ya sign foo"
+puts "Good luck at " + char_age
