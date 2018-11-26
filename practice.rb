5d2505616436b6a67b16f460268091647bee8619
Phillip Cumberlander <cumberlander11@gmail.com>
2018-11-26 11:52:12 -0500

    learned about data types

--- practice.rb
+++ practice.rb
@@ -15,3 +15,10 @@ puts ("He is almost " + char_age + " years old")
 char_name = "Jamal"
 puts (char_name + " whats ya sign foo")
 puts ("Good luck at " + char_age)
+
+# Data types in ruby
+string = "hello world"
+number = 21
+boolean = "true or false"
+# New data type to show variable has no value
+newType = nil
